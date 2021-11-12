9958fc4248c34946623c96dcbe18dc984900b9cb
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2021-11-12 09:37:15 +0000

    Move events collector code to utils.collectors

--- generate_timeline.py
+++ generate_timeline.py
@@ -7,6 +7,8 @@ import re
 
 import jinja2
 
+from utils import collectors
+
 parser = argparse.ArgumentParser(description="Generate a HTML view of Uyuni logs.")
 parser.add_argument(
     "-o",
@@ -70,221 +72,25 @@ data_dict = {
     ],
 }
 
-EXCLUDED_SALT_EVENTS = "minion_event"
-
-##################################################################
-### Salt Event Bus
-
-data_dict["groups"][0]["events"] = []
-
-with open("salt-events.txt") as f:
-    in_data = f.readlines()
-    for i, line in enumerate(in_data):
-        if re.match(r"^.*\t{", line):
-            # Begin tag and json
-            tag, content = line.split("\t")
-            j = i
-            while j < len(in_data) - 1 and in_data[j] != "}\n":
-                content += in_data[j + 1]
-                j += 1
-            try:
-                content = json.loads(content)
-            except Exception as exc:
-                print("Error parsing JSON -> {}".format(content))
-                print(exc)
-                continue
-
-            # Exclude events older than given datetime
-            if args._from and datetime.datetime.fromisoformat(
-                content["_stamp"]
-            ) < datetime.datetime.fromisoformat(args._from):
-                continue
-
-            # Exclude events newer than given datetime
-            if args._until and datetime.datetime.fromisoformat(
-                content["_stamp"]
-            ) > datetime.datetime.fromisoformat(args._until):
-                continue
-
-            new_item = {
-                "id": event_counter,
-                "content": tag,
-                "color": "green",
-                "raw": pprint.pformat(content),
-                "timestamp": content["_stamp"],
-            }
-
-            if tag.startswith("salt/batch"):
-                new_item["type"] = "batch"
-            elif tag.startswith("salt/job"):
-                new_item["type"] = "job"
-            elif re.match(r"^/salt/job/[0-9]+/ret/.*", tag):
-                new_item["type"] = "job_return"
-            elif tag.startswith("salt/auth"):
-                new_item["type"] = "auth"
-            elif tag.startswith("salt/minion"):
-                new_item["type"] = "minion_event"
-            elif tag.startswith("salt/engines/"):
-                new_item["type"] = "minion_event"
-            elif tag.startswith("minion/refresh"):
-                new_item["type"] = "minion_refresh"
-
-            if new_item.get("type", "") not in EXCLUDED_SALT_EVENTS:
-                data_dict["groups"][0]["events"].append(new_item)
-                event_counter += 1
-
-##################################################################
-
-data_dict["groups"][3]["events"] = []
-
-with open("rhn_web_ui.log") as f:
-    in_data = f.readlines()
-
-    for i, line in enumerate(in_data):
-        if re.match(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} .*", line):
-            timestamp, thread, level, content = re.match(
-                r"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}) (\[.*\]) (\w+) (.*)",
-                line,
-            ).groups()
-            if level != "ERROR":
-                continue
-
-            j = i
-            while j < len(in_data) - 1 and not re.match(
-                r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} .*", in_data[j + 1]
-            ):
-                content += in_data[j + 1]
-                j += 1
-
-            datetime_obj = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S,%f")
-
-            # TODO: Adjust timezone
-            datetime_obj = datetime_obj - datetime.timedelta(hours=1)
-
-            # Exclude events older than given datetime
-            if args._from and datetime_obj < datetime.datetime.fromisoformat(
-                args._from
-            ):
-                continue
-
-            # Exclude events rewer than given datetime
-            if args._until and datetime_obj > datetime.datetime.fromisoformat(
-                args._until
-            ):
-                continue
-
-            if (
-                "LoginController - LOCAL AUTH FAILURE:" in content
-                or "common.DownloadFile - " in content
-            ):
-                continue
-
-            new_item = {
-                "id": event_counter,
-                "content": level,
-                "raw": "{} - {}".format(level, content),
-                "color": "orange",
-                "timestamp": timestamp,
-            }
-
-            data_dict["groups"][3]["events"].append(new_item)
-            event_counter += 1
-
-##################################################################
-
-data_dict["groups"][2]["events"] = []
-
-with open("api") as f:
-    in_data = f.readlines()
-
-    for i, line in enumerate(in_data):
-        if re.match(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} .*", line):
-            timestamp, component, level, content = re.match(
-                r"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}) (\[.*\])(\[.*\])(\[.*\].*)",
-                line,
-            ).groups()
-            if "ERROR" in level:
-                continue
-
-            j = i
-            while j < len(in_data) - 1 and not re.match(
-                r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} .*", in_data[j + 1]
-            ):
-                content += in_data[j + 1]
-                j += 1
-
-            datetime_obj = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S,%f")
-
-            # Exclude events older than given datetime
-            if args._from and datetime_obj < datetime.datetime.fromisoformat(
-                args._from
-            ):
-                continue
-
-            # Exclude events rewer than given datetime
-            if args._until and datetime_obj > datetime.datetime.fromisoformat(
-                args._until
-            ):
-                continue
-
-            new_item = {
-                "id": event_counter,
-                "content": level,
-                "raw": "{} - {}".format(component, content),
-                "color": "red",
-                "timestamp": timestamp,
-            }
-
-            data_dict["groups"][2]["events"].append(new_item)
-            event_counter += 1
-
-##################################################################
-
-data_dict["groups"][1]["events"] = []
-
-with open("master") as f:
-    in_data = f.readlines()
-
-    for i, line in enumerate(in_data):
-        if re.match(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} .*", line):
-            timestamp, component, level, content = re.match(
-                r"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}) (\[.*\])(\[.*\])(\[.*\].*)",
-                line,
-            ).groups()
-            if "ERROR" in level:
-                continue
-
-            j = i
-            while j < len(in_data) - 1 and not re.match(
-                r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} .*", in_data[j + 1]
-            ):
-                content += in_data[j + 1]
-                j += 1
-
-            datetime_obj = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S,%f")
-
-            # Exclude events older than given datetime
-            if args._from and datetime_obj < datetime.datetime.fromisoformat(
-                args._from
-            ):
-                continue
-
-            # Exclude events rewer than given datetime
-            if args._until and datetime_obj > datetime.datetime.fromisoformat(
-                args._until
-            ):
-                continue
-
-            new_item = {
-                "id": event_counter,
-                "content": level,
-                "raw": "{} - {}".format(component, content),
-                "color": "blue",
-                "timestamp": timestamp,
-            }
-
-            data_dict["groups"][1]["events"].append(new_item)
-            event_counter += 1
+try:
+    data_dict["groups"][0]["events"] = collectors.from_salt_events(
+        "salt-events.txt", args._from, args._until, event_counter
+    )
+    data_dict["groups"][1]["events"] = collectors.from_salt_master(
+        "master", args._from, args._until, event_counter
+    )
+    data_dict["groups"][2]["events"] = collectors.from_salt_api(
+        "api", args._from, args._until, event_counter
+    )
+    data_dict["groups"][3]["events"] = collectors.from_java_web_ui(
+        "rhn_web_ui.log", args._from, args._until, event_counter
+    )
+    data_dict["groups"][4]["events"] = []
+    data_dict["groups"][5]["events"] = []
+    data_dict["groups"][6]["events"] = []
+except OSError as exc:
+    print("Oops, there was an error when collecting events!")
+    print(exc)
 
 output = template.render(**data_dict)
 
