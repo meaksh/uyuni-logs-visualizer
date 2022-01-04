3c1e9c6e42901eda8950de1ffacb2bd7099a0d83
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-01-04 15:31:07 +0000

    Prepare code to run setup.py script

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="uyuni-logs-visualizer",
+    version="0.1",
+    author="Pablo Suárez Hernández",
+    author_email="psuarezhernandez@suse.com",
+    description="A tool to graphically visualize events from Uyuni logs and Salt events bus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/meaksh/uyuni-logs-visualizer",
+    project_urls={
+        "Bug Tracker": "https://github.com/meaksh/uyuni-logs-visualizer/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+    scripts=["uyuni-logs-visualizer"],
+)
